8309709ef5fa67a58de4f354f0165c258f3801de
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-04 18:01:06 +0530

    completed AuthenticationService section

--- backend/src/main/java/com/platfrom/JobAndSkillDevelopment/entity/User.java
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/entity/User.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Entity
 @Data
 @Table(name = "user")
-@NoArgsConstructor
 @AllArgsConstructor
 public class User implements UserDetails {
     @Id
@@ -30,13 +29,22 @@ public class User implements UserDetails {
     @Column(nullable = false)
     String password;
 
-    boolean enabled;
-
     @Column(name = "verification_code")
     String verificationCode;
 
     @Column(name = "verification_expiration")
-    LocalDateTime verificationExpiration;
+    LocalDateTime verificationCodeExpiresAt;
+    boolean enabled;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(){
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
