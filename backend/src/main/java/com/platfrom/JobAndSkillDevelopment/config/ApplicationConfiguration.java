6de55e7e262dee1a0fdcd04de57bf74fb39177ee
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-04 13:23:07 +0530

    completed ApplicationConfiguration section

--- backend/src/main/java/com/platfrom/JobAndSkillDevelopment/config/ApplicationConfiguration.java
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/config/ApplicationConfiguration.java
@@ -1,8 +1,49 @@
 package com.platfrom.JobAndSkillDevelopment.config;
 
+import com.platfrom.JobAndSkillDevelopment.repo.UserRepo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    private final UserRepo userRepo;
+
+    public ApplicationConfiguration(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 
 }
