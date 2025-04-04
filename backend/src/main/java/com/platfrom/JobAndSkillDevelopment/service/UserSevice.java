c868b67780052ff027a4b1c1e5704270e1d7257b
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-04 18:15:01 +0530

    completed UserService section

---
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/service/UserSevice.java
@@ -0,0 +1,22 @@
+package com.platfrom.JobAndSkillDevelopment.service;
+
+import com.platfrom.JobAndSkillDevelopment.entity.User;
+import com.platfrom.JobAndSkillDevelopment.repo.UserRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+class UserService {
+    private final UserRepo userRepository;
+    public UserService(UserRepo userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
