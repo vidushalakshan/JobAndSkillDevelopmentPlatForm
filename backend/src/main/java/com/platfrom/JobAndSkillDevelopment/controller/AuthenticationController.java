22b96d6aa678b2325cd3891e6d1ae82c1c59fdbd
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-04 18:07:14 +0530

    completed AuthenticationController section

---
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/controller/AuthenticationController.java
@@ -0,0 +1,58 @@
+package com.platfrom.JobAndSkillDevelopment.controller;
+
+import com.platfrom.JobAndSkillDevelopment.dto.LoginUserDto;
+import com.platfrom.JobAndSkillDevelopment.dto.RegisterUserDto;
+import com.platfrom.JobAndSkillDevelopment.dto.VerifyUserDto;
+import com.platfrom.JobAndSkillDevelopment.entity.User;
+import com.platfrom.JobAndSkillDevelopment.responses.LoginResponse;
+import com.platfrom.JobAndSkillDevelopment.service.AuthenticationService;
+import com.platfrom.JobAndSkillDevelopment.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto){
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto) {
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account verified successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestParam String email) {
+        try {
+            authenticationService.resendVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
