e49becd444eec356a1498dc122661ba50b9d450c
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-05 11:49:31 +0530

    completed nav bar section

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,11 +4,11 @@ import ThemeToggle from "./ThemeToggle";
 
 const Nav = () => {
   return (
-    <header className="w-full py-1 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex w-full ">
+    <header className="w-full py-4 sm:px-10 px-5 flex justify-between items-center shadow-md bg-white dark:bg-gray-900">
+      <nav className="flex w-full justify-center items-center screen-max-width">
         <img src={navLogo} alt="Nav Logo" width={120} />
 
-        <div>
+        <div className="flex flex-1 justify-center items-center gap-7 max-sm:hidden">
           <button className="px-6 py-2 rounded-2xl text-white font-semibold shadow-md transition-all bg-red-600 duration-200">
             Employers
           </button>
@@ -18,7 +18,7 @@ const Nav = () => {
           <ThemeToggle />
         </div>
 
-        <div>
+        <div className="flex items-center gap-7 max:sm:flex-1 justify-center ">
           <button className="px-3 py-2 rounded-lg text-white font-semibold shadow-md transition-all bg-orange-400 duration-200">
             <UserPlusIcon className="w-6 h-6 text-gray-600" />
           </button>
