14c1fc26eacc4437e618eaa3379397ee95d9af86
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-05 11:00:00 +0530

    updated content navbar

--- frontend/src/components/NavBar/Nav.jsx
+++ frontend/src/components/NavBar/Nav.jsx
@@ -1,9 +1,33 @@
-import './Nav.css'
+import { navLogo } from "../../utiils";
+import "./Nav.css";
+import { ArrowRightOnRectangleIcon, UserPlusIcon } from "@heroicons/react/24/outline";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header>
+      <nav>
+        <img src={navLogo} alt="Nav Logo" width={120} />
 
-export default Nav
+        <div>
+          <button className="px-6 py-2 rounded-2xl text-white font-semibold shadow-md transition-all bg-red-600 duration-200">
+            Employers
+          </button>
+          <button className="px-9 py-2 rounded-2xl text-white font-semibold shadow-md transition-all bg-orange-400 duration-200">
+            Trainers
+          </button>
+        </div>
+
+        <div>
+          <button className="px-3 py-2 rounded-lg text-white font-semibold shadow-md transition-all bg-orange-400 duration-200">
+            <UserPlusIcon className="w-6 h-6 text-gray-600" />
+          </button>
+          <button className="px-3 py-2 rounded-lg text-white font-semibold shadow-md transition-all bg-orange-400 duration-200">
+            <ArrowRightOnRectangleIcon className="w-6 h-6 text-gray-600" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
