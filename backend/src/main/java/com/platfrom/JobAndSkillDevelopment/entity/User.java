d0068890985830e7ae4dbfae70359d15b822083f
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-10 17:34:11 +0530

    aded JobPost Dto in backend

--- backend/src/main/java/com/platfrom/JobAndSkillDevelopment/entity/User.java
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/entity/User.java
@@ -3,7 +3,6 @@ package com.platfrom.JobAndSkillDevelopment.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
