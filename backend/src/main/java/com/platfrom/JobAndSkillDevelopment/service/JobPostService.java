72dd08d93ec60525e8d37c2b2dd732d0a1cd15c3
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-10 17:55:24 +0530

    updated savJobPost in service getAll,update,delete,findId

--- backend/src/main/java/com/platfrom/JobAndSkillDevelopment/service/JobPostService.java
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/service/JobPostService.java
@@ -4,7 +4,9 @@ import com.platfrom.JobAndSkillDevelopment.dto.JobPostDto;
 import com.platfrom.JobAndSkillDevelopment.entity.JobPost;
 import com.platfrom.JobAndSkillDevelopment.repo.JobPostRepo;
 import jakarta.transaction.Transactional;
+import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +26,26 @@ public class JobPostService {
        jobPostRepo.save(modelMapper.map(jobPostDto, JobPost.class));
        return jobPostDto;
    }
+
+   public List<JobPostDto> getAllJobPosts() {
+       List<JobPost> jobPostList = jobPostRepo.findAll();
+       return modelMapper.map(jobPostList, new TypeToken<List<JobPostDto>>() {}.getType());
+   }
+
+   public JobPostDto updateJobPost(JobPostDto jobPostDto) {
+       jobPostRepo.save(modelMapper.map(jobPostDto, JobPost.class));
+       return jobPostDto;
+   }
+
+   public String deleteJobPost(Long id) {
+       jobPostRepo.deleteById(id);
+       return "Job post deleted..";
+   }
+
+   public JobPostDto getJobPostById(Long id) {
+       JobPost item = jobPostRepo.findJobPostById(id);
+       return modelMapper.map(item, JobPostDto.class);
+   }
+
 
 }
