cb9aead216741d7527dfadb9172311268c6e4f99
vidushalakshan <vidushalakshan7890@gmail.com>
2025-04-11 14:16:42 +0530

    updated

--- backend/src/main/java/com/platfrom/JobAndSkillDevelopment/repo/JobPostRepo.java
+++ backend/src/main/java/com/platfrom/JobAndSkillDevelopment/repo/JobPostRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface JobPostRepo extends JpaRepository<JobPost, Long> {
-    @Query(value = "SELECT * FROM jobpost WHERE id =?1",nativeQuery = true )
+    @Query(value = "SELECT * FROM jobpost WHERE user_id =?1",nativeQuery = true )
     JobPost findJobPostById(Long id);
 }
